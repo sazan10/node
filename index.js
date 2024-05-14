a5b659347c806195cb8af2253870213a5d0467cd
sazan10 <sazzzan10@gmail.com>
2024-05-14 17:40:17 +0545

    execute mongodb to get and store data instead of file

--- index.js
+++ index.js
@@ -23,29 +23,52 @@ const userSchema = new mongoose.Schema({
     gender:{
         type:String
     }
-})
+},{timestamps: true})
 const User = mongoose.model("user", userSchema);
 
 
 app.use(express.urlencoded({extended:false}));
-app.get("/api/users",(req,res)=>{
-    res.json(data);
+app.get("/api/users",async (req,res)=>{
+    const allUsers = await User.find({});
+    res.json(allUsers);
 })
-app.get("/users",(req,res)=>{
+app.get("/users",async (req,res)=>{
+    const allUsers = await User.find({});
     res.send(`<ul>
-        ${data.map(i=>`<li>${i.first_name}</li>`).join("")}
+        ${allUsers.map(i=>`<li>${i.firstName}: - ${i.email}</li>`).join("")}
     </ul>`);
 })
-app.post("/api/register",(req,res)=>{
+app.post("/api/register",async (req,res)=>{
     console.log('req body', req.body)
     data.push({id:data.length+1,...req.body});
-    fs.writeFile('./MOCK_DATA.json',JSON.stringify(data),(err,data)=>{
-        return res.json({status:"success"});
-    });
+    const body = req.body;
+    if(!body || !body.first_name|| !body.last_name || !body.email || !body.gender){
+        res.status(400).json({msg:"All fields are required"});
+    }
+    const result = await User.create({
+        firstName:body.first_name,
+        lastName: body.last_name,
+        email: body.email,
+        gender: body.gender,
+    })
+    return res.status(201).json({status:"success"});
+    // fs.writeFile('./MOCK_DATA.json',JSON.stringify(data),(err,data)=>{
+    //     return res.json({status:"success"});
+    // });
 })
-app.get("/api/users/:id",(req,res)=>{
+app.get("/api/users/:id",async (req,res)=>{
     const userId= Number(req.params.id);
-    const user = data.find(d=>d.id==userId);
+    // const user = data.find(d=>d.id==userId);
+    try{
+        //patch: findByIdAndUpdate(req.params.id, {lastName:"odnfs"})
+        //delete: findByIdAndDelete(req.params.id)
+        const user = await User.findById(req.params.id);
     res.json(user);
+
+    }
+    catch{
+
+        res.json({ms:'not found'});
+    }
 })
 app.listen(3000,(req,res)=>console.log("Server started"));
